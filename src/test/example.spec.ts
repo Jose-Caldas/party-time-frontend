73434940fe6b42054e2ad1bf68d348f5bdf84e6c
Jose Caldas <josecaldas@Joses-MacBook-Pro.local>
2023-04-21 21:13:51 -0300

    add supertest and create first test

--- src/test/example.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from 'vitest'
-
-test('should user can create a new transaction', () => {
-  // fazer a chamada HTTP p/ criar uma nova transação
-  const responseStatusCode = 201
-
-  expect(responseStatusCode).toEqual(201)
-  // validação
-})
