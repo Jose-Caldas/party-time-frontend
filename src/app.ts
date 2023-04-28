ae84b42dddd60c2cbc94657135ce283ea0f0a6b7
Jose Caldas <jrcaldas@yahoo.com.br>
2023-04-28 13:53:55 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
 import { transactionsRoutes } from './routes/transactions'
+import cors from '@fastify/cors'
 
 export const app = fastify()
+
+app.register(cors)
 
 app.register(cookie)
 app.register(transactionsRoutes, {
